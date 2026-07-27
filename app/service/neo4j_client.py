f511ead7ef996fdc1ddb44bb605b134f09e27dfa
WUDUPUPPE <swizz020@icloud.com>
2026-07-27 17:29:12 +0200

    change bolt aadress to getenv variable

--- app/service/neo4j_client.py
+++ app/service/neo4j_client.py
@@ -2,7 +2,7 @@ import os
 from neo4j import GraphDatabase
 
 driver = GraphDatabase.driver(
-    "bolt://neo4j:7678",
+    os.getenv("NEO4J_URI"),
     auth=(os.getenv("NEO4J_USER"), os.getenv("NEO4J_PW"))
 )
 
