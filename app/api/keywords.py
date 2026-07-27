ddc6de066521167d18690b8cefcd72b4f5534d0a
WUDUPUPPE <swizz020@icloud.com>
2026-07-27 15:32:11 +0200

    add typing str to get challenge by keyword as params

--- app/api/keywords.py
+++ app/api/keywords.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from app.service.neo4j_client import run_query
 
 router = APIRouter()
@@ -8,8 +8,8 @@ def get_keywords():
     query = "MATCH (c:Challenge) UNWIND c.keywords AS keyword RETURN DISTINCT keyword"
     return run_query(query)
 
-@router.get("/keywords/{kw}/challenges", tags=["Graph-Info"])
-def get_challenges_by_keyword(kw: str):
+@router.get("/keywords/challenges", tags=["Graph-Info"])
+def get_challenges_by_keyword(kw: str = Query):
     query = "MATCH (c:Challenge) WHERE $kw IN c.keywords RETURN c"
     return run_query(query, {"kw": kw})
 
