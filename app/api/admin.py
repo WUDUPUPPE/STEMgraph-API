af63306c574aeaccf45529740191c08048dd16be
WUDUPUPPE <swizz020@icloud.com>
2026-07-29 12:44:56 +0200

    add pydantic to admin and updated API Version

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,12 +1,19 @@
 import os
+from pydantic import BaseModel
 from fastapi import APIRouter, Header, HTTPException
 from app.service.neo4j_client import run_query
 
+class UpdateResponse(BaseModel):
+    status: str
+    message: str
+    
 router = APIRouter()
 WRITE_TOKEN = os.getenv("WRITE_TOKEN")
 
 @router.post("/admin/update-challenges", tags=["Admin/Health Check"])
-def update_challenges(x_api_key: str = Header(...)):
+def update_challenges(x_api_key: str = Header(...)) -> UpdateResponse:
     if x_api_key != WRITE_TOKEN:
         raise HTTPException(status_code=403, detail="Invalid API Key")
-    return {"status": "ok", "message": "Challenges updated"}
+    return UpdateResponse(
+        status="ok", message="Challenges successfully updated and generated JSON"
+    )
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.api import graph, challenges, keywords, admin
 
 
-app = FastAPI(title="STEMgraph Challenge API", version="1.0.1")
+app = FastAPI(title="STEMgraph Challenge API", version="1.0.2")
 
 app.include_router(graph.router)
 app.include_router(challenges.router)
