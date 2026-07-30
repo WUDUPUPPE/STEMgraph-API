8ed0f27e2b54b844c4ca1209c77db6b77727c27c
WUDUPUPPE <swizz020@icloud.com>
2026-07-30 13:03:48 +0200

    add Dependency endpoint

--- app/api/graph.py
+++ app/api/graph.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.models.schema import SubgraphGraphResponse
+from app.models.schema import SubgraphGraphResponse, DependencyGraphResponse
 from app.service.neo4j_client import run_query
     
 router = APIRouter()
@@ -12,6 +12,27 @@ def get_graph():
     RETURN DISTINCT c.uuid AS SOURCE, c.title AS source_TITLE, dep.uuid AS TARGET, dep.title AS target_TITLE
     """
     return run_query(query)
+
+#Abhängige Challenges als Graph
+@router.get("/challenges/{id}/depends-on/graph", tags=["Graph-Info"])
+def get_challenge_dependencies_graph(id: str) -> DependencyGraphResponse:
+    node_query = """
+    MATCH (c:Challenge {uuid: $id})-[:BUILDS_ON*]->(dep:Challenge)
+    RETURN DISTINCT dep.uuid AS UUID, dep.title AS TITLE
+    """
+
+    edge_query = """
+    MATCH (c:Challenge {uuid: $id})-[:BUILDS_ON*]->(a:Challenge)
+    MATCH (a)-[:BUILDS_ON]->(b:Challenge)
+    RETURN DISTINCT a.uuid AS SOURCE, b.uuid AS TARGET
+    """
+
+    nodes = run_query(node_query, {"id": id})
+    edges = run_query(edge_query, {"id": id})
+
+    return {
+        "nodes": nodes, "edges": edges
+    }
 
 @router.get("/subgraph/graph", tags=["Graph-Info"])
 def get_subgraph_graph(start: str, end: str) -> SubgraphGraphResponse:
