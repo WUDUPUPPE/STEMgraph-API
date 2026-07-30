75210390f64733e3df64a697ec2187e24a307452
WUDUPUPPE <swizz020@icloud.com>
2026-07-30 12:45:08 +0200

    add get graph as graph

--- app/models/schema.py
+++ app/models/schema.py
@@ -1,15 +1,18 @@
 from pydantic import BaseModel, Field
 
+#All Challenges AS List
 class ChallengeListResponse(BaseModel):
     uuid: str
     title: str | None = None
     keywords: list[str] = Field(default_factory=list)
 
+#Challenges Dependency AS List
 class DependencyResponse(BaseModel):
     uuid: str
     title: str | None = None
     keywords: list[str] = Field(default_factory=list)
 
+#Challenges Dependency AS Graph
 class GraphNodeResponse(BaseModel):
     uuid: str
     title: str | None = None
@@ -21,7 +24,8 @@ class GraphEdgeResponse(BaseModel):
 class DependencyGraphResponse(BaseModel):
     nodes: list[GraphNodeResponse] = Field(default_factory=list)
     edges: list[GraphEdgeResponse] = Field(default_factory=list)
-    
+
+#Challenge Previous and Next for Pop-Up
 class NeighborChallenge(BaseModel):
     uuid: str
     title: str | None = None
@@ -30,8 +34,23 @@ class NeighborChallenge(BaseModel):
 class NeighborsResponse(BaseModel):
     previous: list[NeighborChallenge] = Field(default_factory=list)
     next: list[NeighborChallenge] = Field(default_factory=list)
-    
+
+#Subgraph AS List
 class SubgraphListResponse(BaseModel):
     uuid: str
     title: str 
     keywords: list[str] = Field(default_factory=list)
+
+#Subgraph AS Graph
+class GraphNode(BaseModel):
+    uuid: str
+    title: str | None = None
+    keywords: list[str] = Field(default_factory=list)
+
+class GraphEdge(BaseModel):
+    source: str
+    target: str
+
+class SubgraphGraphResponse(BaseModel):
+    nodes: list[GraphNode] = Field(default_factory=list)
+    edges: list[GraphEdge] = Field(default_factory=list)
