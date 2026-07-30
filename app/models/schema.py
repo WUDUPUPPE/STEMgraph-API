847a83b044cef72a1083643e8b6558a2ca2b505e
WUDUPUPPE <swizz020@icloud.com>
2026-07-30 13:14:01 +0200

    add get graph As graph

--- app/models/schema.py
+++ app/models/schema.py
@@ -1,16 +1,24 @@
 from pydantic import BaseModel, Field
 
+#All Challenges AS Graph
+class Node(BaseModel):
+    uuid: str
+    title: str | None = None
+
+class Edge(BaseModel):
+    source: str
+    target: str
+
+class GraphResponse(BaseModel):
+    nodes: list[Node]
+    edges: list[Edge]
+    
 #All Challenges AS List
 class ChallengeListResponse(BaseModel):
     uuid: str
     title: str | None = None
     keywords: list[str] = Field(default_factory=list)
 
-#Challenges Dependency AS List
-class DependencyResponse(BaseModel):
-    uuid: str
-    title: str | None = None
-    keywords: list[str] = Field(default_factory=list)
 
 #Challenges Dependency AS Graph
 class GraphNodeResponse(BaseModel):
@@ -24,22 +32,13 @@ class GraphEdgeResponse(BaseModel):
 class DependencyGraphResponse(BaseModel):
     nodes: list[GraphNodeResponse] = Field(default_factory=list)
     edges: list[GraphEdgeResponse] = Field(default_factory=list)
-
-#Challenge Previous and Next for Pop-Up
-class NeighborChallenge(BaseModel):
+    
+#Challenges Dependency AS List
+class DependencyResponse(BaseModel):
     uuid: str
     title: str | None = None
     keywords: list[str] = Field(default_factory=list)
 
-class NeighborsResponse(BaseModel):
-    previous: list[NeighborChallenge] = Field(default_factory=list)
-    next: list[NeighborChallenge] = Field(default_factory=list)
-
-#Subgraph AS List
-class SubgraphListResponse(BaseModel):
-    uuid: str
-    title: str 
-    keywords: list[str] = Field(default_factory=list)
 
 #Subgraph AS Graph
 class GraphNode(BaseModel):
@@ -54,3 +53,20 @@ class GraphEdge(BaseModel):
 class SubgraphGraphResponse(BaseModel):
     nodes: list[GraphNode] = Field(default_factory=list)
     edges: list[GraphEdge] = Field(default_factory=list)
+
+#Subgraph AS List
+class SubgraphListResponse(BaseModel):
+    uuid: str
+    title: str 
+    keywords: list[str] = Field(default_factory=list)
+
+
+#Challenge Previous and Next for Pop-Up
+class NeighborChallenge(BaseModel):
+    uuid: str
+    title: str | None = None
+    keywords: list[str] = Field(default_factory=list)
+
+class NeighborsResponse(BaseModel):
+    previous: list[NeighborChallenge] = Field(default_factory=list)
+    next: list[NeighborChallenge] = Field(default_factory=list)
