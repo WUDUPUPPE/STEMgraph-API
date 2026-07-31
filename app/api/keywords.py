9bf8cf738865663bee1ff425f7591f7451e1d26b
WUDUPUPPE <swizz020@icloud.com>
2026-07-31 11:44:32 +0200

    change tag name

--- app/api/keywords.py
+++ app/api/keywords.py
@@ -3,12 +3,12 @@ from app.service.neo4j_client import run_query
 
 router = APIRouter()
 
-@router.get("/keywords", tags=["Graph-Info"])
+@router.get("/keywords", tags=["Keyword-Info"])
 def get_keywords():
     query = "MATCH (c:Challenge) UNWIND c.keywords AS keyword RETURN DISTINCT keyword"
     return run_query(query)
 
-@router.get("/keywords/challenges", tags=["Graph-Info"])
+@router.get("/keywords/challenges", tags=["Keyword-Info"])
 def get_challenges_by_keyword(kw: str = Query):
     query = "MATCH (c:Challenge) WHERE $kw IN c.keywords RETURN c"
     return run_query(query, {"kw": kw})
